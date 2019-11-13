b7abc37abea27c45345f0537471fee361699d1f3
Girbacea Marina <girbaceamarina@gmail.com>
2019-11-13 15:54:30 +0200

    a few updates

--- src/app/common/form/DateInput.jsx
+++ src/app/common/form/DateInput.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { Form, Label } from "semantic-ui-react";
-import DatePicker from "react-datepicker";
-import 'react-datepicker/dist/react-datepicker.css'
+import React from 'react';
+import { Form, Label } from 'semantic-ui-react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const DateInput = ({
   input,
@@ -17,13 +17,12 @@ const DateInput = ({
         placeholderText={placeholder}
         selected={input.value ? new Date(input.value) : null}
         onChange={input.onChange}
+        onBlur={input.onBlur}
+        onChangeRaw={(e) => e.preventDefault()}
       />
-      {touched && error && (
-        <Label basic color="red">
-          {error}
-        </Label>
-      )}
+      {touched && error && <Label basic color='red'>{error}</Label>}
     </Form.Field>
   );
 };
+
 export default DateInput;
--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -13,6 +13,7 @@ import cuid from "cuid";
 import TextInput from "../../../app/common/form/TextInput";
 import TextArea from "../../../app/common/form/TextArea";
 import SelectInput from "../../../app/common/form/SelectInput";
+import DateInput from "../../../app/common/form/DateInput";
 
 const mapState = (state, ownProps) => {
   const eventId = ownProps.match.params.id;
@@ -117,9 +118,12 @@ class EventForm extends Component {
                 placeholder="Event Venue"
               />
               <Field
-                name="date"
-                component={TextInput}
-                placeholder="Event Date"
+                name='date'
+                component={DateInput}
+                dateFormat='dd LLL yyyy h:mm a'
+                showTimeSelect
+                timeFormat='HH:mm'
+                placeholder='Event Date'
               />
 
               <Button disabled={invalid || submitting || pristine } positive type="submit">
