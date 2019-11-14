ba3d30fbb0d352b3702085a25942c7dfb7ecd896
Girbacea Marina <girbaceamarina@gmail.com>
2019-11-14 16:32:33 +0200

    -Adding a Loading Component -Resolve the issus with our login modal -Hide some things for the people that are not loged in

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -1,34 +1,34 @@
-import React, { Component } from 'react';
-import { Grid } from 'semantic-ui-react';
-import {connect} from 'react-redux';
-import EventList from '../EventList/EventList';
-import {createEvent, deleteEvent, updateEvent} from '../eventActions'
+import React, { Component } from "react";
+import { Grid } from "semantic-ui-react";
+import { connect } from "react-redux";
+import EventList from "../EventList/EventList";
+import { createEvent, deleteEvent, updateEvent } from "../eventActions";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
 
-const mapState = (state) => ({
-  events: state.events
-})
+const mapState = state => ({
+  events: state.events,
+  //loading: state.async.loading
+});
 
 const actions = {
-  createEvent, 
+  createEvent,
   deleteEvent,
   updateEvent
-}
+};
 
 class EventDashboard extends Component {
-
   handleDeleteEvent = id => {
     this.props.deleteEvent(id);
   };
 
   render() {
-    const {events} = this.props;
+    const{events}=this.props
+    //const {events, loading} = this.props;
+  //  if (loading) return <LoadingComponent />
     return (
       <Grid>
         <Grid.Column width={10}>
-          <EventList
-            events={events}
-            deleteEvent={this.handleDeleteEvent}
-          />
+          <EventList events={events} deleteEvent={this.handleDeleteEvent} />
         </Grid.Column>
         <Grid.Column width={6}>
           <h2>Activity Feed</h2>
--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -1,56 +1,63 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Menu, Container, Button } from 'semantic-ui-react';
-import { NavLink, Link, withRouter } from 'react-router-dom';
-import SignedOutMenu from '../Menus/SignedOutMenu';
-import SignedInMenu from '../Menus/SignedInMenu';
-import { openModal } from '../../modals/modalActions';
-import {logout} from '../../auth/authActions'
+import React, { Component, Fragment } from "react";
+import { connect } from "react-redux";
+import { Menu, Container, Button } from "semantic-ui-react";
+import { NavLink, Link, withRouter } from "react-router-dom";
+import SignedOutMenu from "../Menus/SignedOutMenu";
+import SignedInMenu from "../Menus/SignedInMenu";
+import { openModal } from "../../modals/modalActions";
+import { logout } from "../../auth/authActions";
 
 const actions = {
   openModal,
   logout
 };
 
-const mapState = (state) => ({
+const mapState = state => ({
   auth: state.auth
-})
+});
 
 class NavBar extends Component {
-  handleSignIn = () => this.props.openModal('LoginModal');
+  handleSignIn = () => this.props.openModal("LoginModal");
 
-  handleRegister = () => this.props.openModal('RegisterModal');
+  handleRegister = () => this.props.openModal("RegisterModal");
 
   handleSignOut = () => {
-    this.props.logout()
-    this.props.history.push('/');
+    this.props.logout();
+    this.props.history.push("/");
   };
 
   render() {
     const { auth } = this.props;
     const authenticated = auth.authenticated;
     return (
-      <Menu inverted fixed='top'>
+      <Menu inverted fixed="top">
         <Container>
-          <Menu.Item as={NavLink} exact to='/' header>
-            <img src='/assets/logo.png' alt='logo' />
+          <Menu.Item as={NavLink} exact to="/" header>
+            <img src="/assets/logo.png" alt="logo" />
             Re-vents
           </Menu.Item>
-          <Menu.Item as={NavLink} exact to='/events' name='Events' />
-          <Menu.Item as={NavLink} to='/people' name='People' />
-          <Menu.Item as={NavLink} to='/test' name='Test' />
-          <Menu.Item>
-            <Button
-              as={Link}
-              to='/createEvent'
-              floated='right'
-              positive
-              inverted
-              content='Create Event'
-            />
-          </Menu.Item>
+          <Menu.Item as={NavLink} exact to="/events" name="Events" />
+          {authenticated && (
+            <Fragment>
+              <Menu.Item as={NavLink} to="/people" name="People" />
+              <Menu.Item as={NavLink} to="/test" name="Test" />
+              <Menu.Item>
+                <Button
+                  as={Link}
+                  to="/createEvent"
+                  floated="right"
+                  positive
+                  inverted
+                  content="Create Event"
+                />
+              </Menu.Item>
+            </Fragment>
+          )}
           {authenticated ? (
-            <SignedInMenu signOut={this.handleSignOut} currentUser={auth.currentUser} />
+            <SignedInMenu
+              signOut={this.handleSignOut}
+              currentUser={auth.currentUser}
+            />
           ) : (
             <SignedOutMenu
               signIn={this.handleSignIn}
@@ -63,9 +70,4 @@ class NavBar extends Component {
   }
 }
 
-export default withRouter(
-  connect(
-    mapState,
-    actions
-  )(NavBar)
-);
+export default withRouter(connect(mapState, actions)(NavBar));
