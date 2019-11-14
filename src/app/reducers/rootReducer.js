ddbb7c286af83332113fcee74e4d1be2ccf65a2f
Girbacea Marina <girbaceamarina@gmail.com>
2019-11-14 11:40:17 +0200

    Setting up our modals in Redux

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,12 +1,14 @@
 import { combineReducers } from "redux";
-import {reducer as FormReducer} from 'redux-form';
+import { reducer as FormReducer } from "redux-form";
 import testReducer from "../../features/testarea/testReducer";
 import eventReducer from "../../features/event/eventReducer";
+import modalReducer from "../../features/modals/modalReducer";
 
 const rootReducer = combineReducers({
-    form: FormReducer,
-    test: testReducer,
-    events: eventReducer
-})
+  form: FormReducer,
+  test: testReducer,
+  events: eventReducer,
+  modals: modalReducer
+});
 
 export default rootReducer;
--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -1,29 +1,43 @@
-import React, { Component } from 'react'
-import {connect} from 'react-redux';
-import {incrementCounter, decrementCounter} from './testActions'
-import { Button } from 'semantic-ui-react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { incrementCounter, decrementCounter } from "./testActions";
+import { Button } from "semantic-ui-react";
+import TestPlaceInput from "./TestPlaceInput";
+import { openModal } from "../modals/modalActions";
 
-const mapState = (state) => ({
+const mapState = state => ({
   data: state.test.data
-})
+});
 
 const actions = {
   incrementCounter,
-  decrementCounter
-}
+  decrementCounter,
+  openModal
+};
 
 class TestComponent extends Component {
   render() {
-    const {data, incrementCounter, decrementCounter} = this.props;
+    const { data, incrementCounter, decrementCounter } = this.props;
     return (
       <div>
         <h1>Test Component</h1>
         <h3>The answer is: {data}</h3>
-        <Button onClick={incrementCounter} positive content='Increment' />
-        <Button onClick={decrementCounter} negative content='Decrement' />
+        <Button onClick={incrementCounter} positive content="Increment" />
+        <Button onClick={decrementCounter} negative content="Decrement" />
+
+        <Button
+          onClick={() => openModal("TestModal", { data: 42 })}
+          color="teal"
+          content="Open Modal"
+        />
+
+        <br />
+        <br />
+        <br />
+        <TestPlaceInput></TestPlaceInput>
       </div>
-    )
+    );
   }
 }
 
-export default connect(mapState, actions)(TestComponent)
+export default connect(mapState, actions)(TestComponent);
